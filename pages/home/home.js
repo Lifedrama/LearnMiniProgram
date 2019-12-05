3bbbfc9f944722ec56e6743b12fcb779e36c4d31
lifedrama <15563147639@163.com>
2019-12-05 11:53:57 +0800

    3注册小程序

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,81 @@
 // pages/home/home.js
+//getApp() 获取APP()产生的实例对象
+const app = getApp()
+console.log(app.globaData.name)
+console.log(app.globaData.age)
+
+
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{ 
+    name:"ddd",
+    age:18,
+    student:[
+      {id: 110,name:'ko',age:30},
+      {id: 111, name: 'do', age: 30 },
+      {id: 1120, name: '2o', age: 30 }
+    ],
+    c:0
+  },
+  handleBtnClick(){
+    this.setData({
+      c:this.data.c +1
+    })
+  },
+  handleSubtraction(){
+    this.setData({
+      c:this.data.c - 1
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handclick(event){
+    console.log("————")
+    console.log(event)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //监听其他事件
+  //监听页面滚动
+  onPageScroll(obj){
+    console.log(obj)
   },
+  //监听页面滚动到底部
+onReachBottom(){
+  console.log("页面滚动到底部")
+},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+onPullDownRefresh(){
+  console.log("下拉刷新的事件")
+},
 
+
+
+  //1.监听页面的生命周期函数
+  //页面被加载出来
+  onLoad() {
+    console.log("onLoab")
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      //ES6语法：箭头函数中的this是一层层向上找
+      success: (res) =>{
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //页面显示出来时
+  onShow() {
+    console.log("onShow")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //页面初次渲染完成时 
+  onReady() {
+    console.log("onReady")
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  //当页面隐藏起来时
+  onHide() {
+    console.log("onHide")
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //
+  onUnload(){
+    console.log("onUnload")
   }
 })
+
